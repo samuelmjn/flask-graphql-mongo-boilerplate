d4cf40ebaaa0001ca634f683fdeb18eaed621e18
msanvarov <salim.anvarov@yahoo.com>
2019-09-27 16:39:07 -0400

    :art: added CORS and moved the intialization to manage file

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask
 from flask_graphql import GraphQLView
-from database import mock
+from flask_cors import CORS
 from global_schema import global_schema
 
 app = Flask(__name__)
+CORS(app)
 app.debug = True
 
 app.add_url_rule(
     '/graphql',
     view_func=GraphQLView.as_view('graphql', schema=global_schema, graphiql=True)
 )
-
-if __name__ == '__main__':
-    mock(2)
-    app.run()
