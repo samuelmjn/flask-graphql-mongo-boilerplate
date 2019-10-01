2a4c0f3df01f48516e681db4d47c69f7a94f4759
msanvarov <salim.anvarov@yahoo.com>
2019-10-01 14:42:26 -0400

    :white_check_mark: updated e2e tests

--- test_e2e.py
+++ test_e2e.py
@@ -1,13 +1,12 @@
-# test_e2e.py
-import requests
-import urllib.parse
-from random import choice
 import string
+import pytest
+from __init__ import app
+from random import choice
 from constants import CONFIGS
-from models import profile_collections
+from utils.model import profile_collections
 
-# Please make sure to start the server before starting the e2e test suite
 url = CONFIGS.get("APP_URL")
+client = app.test_client()
 
 
 def test_root_endpoint():
@@ -15,11 +14,10 @@ def test_root_endpoint():
     / endpoint (GET)
     Fetches root route
     """
-    req = requests.get("{}/".format(url))
-    assert req.status_code == 200, "failed get request"
-    print(req.content)
-    assert req.content.decode(
-        "utf-8") == "GraphQL server is listening on /graphql"
+    global client
+    res = client.get("/")
+    assert res.status_code == 200, "failed get resuest"
+    assert res.data == b"GraphQL server is listening on /graphql"
 
 
 def test_registration():
@@ -27,17 +25,16 @@ def test_registration():
     /graphql endpoint (GET)
     Registers a test account
     """
-    query = """
-    {
+    global client
+    query = {"query": """{
       register(email: "test@test.com", password: "password", username: "test", name: "test") {
         email
         token
       }
-    }
-    """
-    req = requests.get("{}/graphql?query={}".format(url,
-                                                    urllib.parse.quote(query)))
-    assert req.status_code == 200
+    }"""}
+    res = client.post("{}/graphql".format(url), json=query)
+    assert res.status_code == 200
+    assert res.json["data"]
 
 
 def test_profile_exists():
@@ -45,19 +42,17 @@ def test_profile_exists():
     /graphql endpoint (GET)
     Tries to re-register the same account
     """
-    query = """
-    {
+    global client
+    query = {"query": """{
       register(email: "test@test.com", password: "password", username: "test", name: "test") {
         email
         token
       }
-    }
-    """
-    req = requests.get("{}/graphql?query={}".format(url,
-                                                    urllib.parse.quote(query)))
-
-    assert len(req.json()["errors"]) == 1
-    assert req.json()["errors"][0][
+    }"""}
+    res = client.post("{}/graphql".format(url), json=query)
+    print(res.json)
+    assert len(res.json["errors"]) == 1
+    assert res.json["errors"][0][
                "message"] == "Profile with that username currently exists in the system."
 
 
@@ -66,17 +61,17 @@ def test_login():
     /graphql endpoint (GET)
     Login's to created account
     """
-    query = """
+    global client
+    query = {"query": """
     {
       login(username: "test", password: "password") {
         email
         token
-      } 
+      }
     }
-    """
-    req = requests.get("{}/graphql?query={}".format(url,
-                                                    urllib.parse.quote(query)))
-    assert req.status_code == 200
+    """}
+    res = client.post("{}/graphql".format(url), json=query)
+    assert res.status_code == 200
 
 
 def test_profile():
@@ -84,24 +79,22 @@ def test_profile():
     /graphql endpoint (GET)
     Token authentication to fetch profile data
     """
-    query_profile = """
+    global client
+    query_profile = {"query": """
        {
          login(username: "test", password: "password") {
            email
            token
-         } 
+         }
        }
-   """
-    req = requests.get("{}/graphql?query={}".format(
-        url, urllib.parse.quote(query_profile)))
-
-    token = req.json()['data']['login']['token']
-
-    query = "{profile(token: \"" + token + "\") { email name username " \
-                                           "isVerified }} "
-    req = requests.get("{}/graphql?query={}".format(url,
-                                                    urllib.parse.quote(query)))
-    assert req.status_code == 200
+   """}
+    res = client.post("{}/graphql".format(url), json=query_profile)
+    token = res.json['data']['login']['token']
+    query = {
+        "query": "{profile(token: \"" + token + "\") { email name username " \
+                                                "isVerified }}"}
+    res = client.post("{}/graphql".format(url), json=query)
+    assert res.status_code == 200
 
 
 def test_login_wrong_password():
@@ -109,20 +102,18 @@ def test_login_wrong_password():
     /graphql endpoint (GET)
     Tries to login with false credentials
     """
-    query = """
-    {
-      login(username: "test", password: "wrong") {
-        email
-        token
-      } 
-    }
-    """
-    r = requests.get("{}/graphql?query={}".format(url,
-                                                  urllib.parse.quote(query)))
-
-    assert len(r.json()['errors']) == 1
-    assert r.json()['errors'][0][
-               'message'] == "The password is incorrect. Please try again."
+    global client
+    query = {"query": """
+      {
+        login(username: "test", password: "wrong") {
+          email
+          token
+        }
+      }
+      """}
+    client.post("{}/graphql".format(url), json=query)
+    with pytest.raises(Exception) as exception:
+        assert exception == "The password is incorrect. Please try again."
 
 
 def test_long_no_profile():
@@ -130,32 +121,37 @@ def test_long_no_profile():
     /graphql endpoint (GET)
     Deletes and tries to fetch deleted account
     """
-    profile_collections.delete_one({"username": "test"})
-    query = """
+    global client
+    test_remove_created_account()
+    query = {"query": """"
     {
       login(username: "test", password: "I don't exist") {
         email
         token
-      } 
+      }
     }
-    """
-    req = requests.get("{}/graphql?query={}".format(url,
-                                                    urllib.parse.quote(query)))
-
-    assert len(req.json()['errors']) == 1
-    assert req.json()['errors'][0][
-               'message'] == "The profile by the username test, was not found in the system."
+    """}
+    client.post("{}/graphql".format(url), json=query)
+    with pytest.raises(Exception) as exception:
+        assert exception == "The profile by the username test, was not found in the system."
 
 
 def test_user_wrong_token():
+    """ Test trying to authenticate with an unsigned token """
+    global client
+
     def random(len=10):
         """Generate a random string of fixed length """
         letters = string.ascii_lowercase
         return ''.join(choice(letters) for _ in range(len))
 
-    query = "{profile(token: \"" + random(
-        50) + "\") { email name username isVerified }}"
-    r = requests.get("{}/graphql?query={}".format(url,
-                                                  urllib.parse.quote(query)))
-    assert len(r.json()['errors']) == 1
-    assert r.json()['errors'][0]['message'] == "Token is invalid."
+    query = {"query": "{profile(token: \"" + random(
+        50) + "\") { email name username isVerified }}"}
+    client.post("graphql".format(url), json=query)
+    with pytest.raises(Exception) as exception:
+        assert exception == "Token is invalid."
+
+
+def test_remove_created_account():
+    """ Drop the test profile from database """
+    assert profile_collections.delete_one({"username": "test"})
